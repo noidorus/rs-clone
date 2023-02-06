65addbb310c93f6f1bfe9e220a9c54c42f63e2a0
HuffPuffTuff <mrsalevan@gmail.com>
2023-02-06 16:39:26 +0400

    fix: prettier and eslint configs

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
 console.log('hahahah');
+const root = document.getElementById('root') as HTMLDivElement;
 
 const elem = (
   <div>
@@ -10,4 +11,4 @@ const elem = (
   </div>
 );
 
-hydrateRoot(document.getElementById('root'), elem);
+hydrateRoot(root, elem);
