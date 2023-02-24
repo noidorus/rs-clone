af11d04901bef08af6d2164986020ed8f034ef82
Szyrwel <potato_father@list.ru>
2023-02-24 21:12:09 +0300

    featL add function shuffle array

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -22,3 +22,11 @@ export function getRelativeTimeString(date: number, lang = navigator.language) {
     return rtf.format(Math.floor(deltaSeconds / divisor), units[unitIndex]);
   }
 }
+
+export function shuffle(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
